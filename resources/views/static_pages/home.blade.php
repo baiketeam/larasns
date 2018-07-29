f2943a9e6fa0e9d29cf697c5fff61d53775f0e14
zhanghanwei <33750720@qq.com>
2018-07-29 07:03:35 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>LaraSNS</h1>
+    <p class="lead">
+      Hello,欢迎来到<a href="https://weibo.com/baiketeamceo">LaraSNS</a> 一个类似新浪微博的社交网站!
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
