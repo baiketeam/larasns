23d1e441dff59510c3c4367ca1041bc85a52c877
zhanghanwei <33750720@qq.com>
2018-07-31 05:20:48 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,18 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    // 使用中间件进行用户权限控制
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create'],
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -37,5 +49,46 @@ class UsersController extends Controller
         session()->flash('success', '注册成功,欢迎来到LaraSNS!');
 
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->authorize('update', $user);
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人信息更新成功');
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户');
+        return back();
     }
 }
